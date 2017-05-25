964cd92061beec17b7e225fe7de7b265e850a428
weifeiyu <weifeiyu@cmcaifu.com>
2017-05-25 16:56:47 +0800

    事务中的本类事务处理

--- src/main/java/com/wfy/server/service/impl/ComputerServiceImpl.java
+++ src/main/java/com/wfy/server/service/impl/ComputerServiceImpl.java
@@ -3,6 +3,7 @@ package com.wfy.server.service.impl;
 import com.wfy.server.dao.ComputerDao;
 import com.wfy.server.model.Computer;
 import com.wfy.server.service.ComputerService;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +24,18 @@ public class ComputerServiceImpl implements ComputerService{
     private ComputerDao computerDao;
 
     public List<Computer> queryObjList(){
+
+        /*try { // 本类中的事务
+            if (AopContext.currentProxy() != null) {
+                ComputerServiceImpl proxy = (ComputerServiceImpl) AopContext.currentProxy();
+                proxy.transactionalHandleUserRegistryResult(user);
+            } else {
+                transactionalHandleUserRegistryResult(user);
+            }
+        }catch (Exception e){
+            //RedisShardPool.sadd(BusinessServerParameter.REDIS_OPENING_EBANK_NUMBER_KEY, user.getBankAccountSerialNumber());
+            logger.error("用户:{},开户成功，但修改数据库异常",user.getPhoneNumber());
+        }*/
 
         return computerDao.queryObjList();
     }
