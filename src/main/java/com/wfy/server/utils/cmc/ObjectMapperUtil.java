d7b7b398586b775e3e1574012e8a390075aa121c
unknown <>
2017-06-14 16:16:39 +0800

    del

--- src/main/java/com/wfy/server/utils/cmc/ObjectMapperUtil.java
+++
@@ -1,9 +0,0 @@
-package com.wfy.server.utils.cmc;
-
-public class ObjectMapperUtil {
-	private static ObjectMapperConfig mapper = new ObjectMapperConfig();
-
-	public static ObjectMapperConfig getJsonMapper() {
-		return mapper;
-	}
-}
